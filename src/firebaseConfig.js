11982ff50f1a488c3fb4b02ec5ff79775cde2990
selemondev <selemondev19@gmail.com>
2022-07-16 18:34:55 +0300

    update the config keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyBTTHqj3CtO7e7hWCtzXZQLtnWw2mMu4zc",
+  authDomain: "vue-twitter-clone-ffd96.firebaseapp.com",
+  projectId: "vue-twitter-clone-ffd96",
+  storageBucket: "vue-twitter-clone-ffd96.appspot.com",
+  messagingSenderId: "26119329874",
+  appId: "1:26119329874:web:dbb33dfec6596fdfb9278b",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
