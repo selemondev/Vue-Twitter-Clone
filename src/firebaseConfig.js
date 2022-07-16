a8f14980a12e1381336485fce39233ce0e63b878
selemondev <selemondev19@gmail.com>
2022-07-16 19:15:25 +0300

    keys update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,19 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-const API_KEY = "AIzaSyBTTHqj3CtO7e7hWCtzXZQLtnWw2mMu4zc";
-const AUTH_DOMAIN = "vue-twitter-clone-ffd96.firebaseapp.com";
-const PROJECT_ID = "vue-twitter-clone-ffd96";
-const STORAGE_BUCKET = "vue-twitter-clone-ffd96.appspot.com";
-const MESSAGING_SENDER_ID = "26119329874";
-const APP_ID = "1:26119329874:web:dbb33dfec6596fdfb9278b";
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: "AIzaSyBTTHqj3CtO7e7hWCtzXZQLtnWw2mMu4zc",
+  authDomain: "vue-twitter-clone-ffd96.firebaseapp.com",
+  projectId: "vue-twitter-clone-ffd96",
+  storageBucket: "vue-twitter-clone-ffd96.appspot.com",
+  messagingSenderId: "26119329874",
+  appId: "1:26119329874:web:dbb33dfec6596fdfb9278b",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
